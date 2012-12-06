5273694e9cf12e7859a2d8a0dd995f5c5bfc8025
Tiago Cardoso <tiagocardosos@gmail.com>
2012-12-06 19:41:32 -0200

    Alteração de arquivo inidividual

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+//Comentário correto para o workshopgit
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../yii-1.1.12.b600af/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
